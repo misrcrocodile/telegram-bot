3e8f250ac647af60c42c44bcfc0406b380432074
misr <misr.crocodile@gmail.com>
2021-12-02 23:52:20 +0900

    add content

---
+++ main.py
@@ -0,0 +1,15 @@
+import telebot
+import os
+
+API_KEY = os.getenv("API_KEY")
+
+bot = telebot.TeleBot(API_KEY, parse_mode=None)
+
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    print(message)
+    bot.reply_to(message, "Howdy, how are you doing?")
+
+
+bot.infinity_polling()
